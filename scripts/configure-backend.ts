5c7ba2af268cc95f237750a4f0a8839db46c2a6f
Karolis Astrauka <karolisa@wix.com>
2021-05-12 16:33:13 +0300

    Sync backend-api file

--- scripts/configure-backend.ts
+++ scripts/configure-backend.ts
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+
 import { syncDirectory } from './utils/sync-directory';
+import { getDirectoryFromCaller } from './utils/paths';
 
 export async function configureBackend() {
+  const backendDst = getDirectoryFromCaller('./dist/backend');
+  fs.renameSync(`${backendDst}/backend-api.js`, `${backendDst}/backend-api.jsw`);
   await syncDirectory('backend', 'src/backend');
 }
--- scripts/utils/sync-directory.ts
+++ scripts/utils/sync-directory.ts
@@ -7,6 +7,8 @@ import { rsync } from './rsync';
 export async function syncDirectory(from: string, to: string) {
   const compiledSource = getDirectoryFromCaller(`./dist/${to}`);
   await del(compiledSource);
-  await copy(getDirectoryFromCaller(`./dist/${from}`), compiledSource, { filter: ['**/*.js', '!**/*.spec.js'] });
+  await copy(getDirectoryFromCaller(`./dist/${from}`), compiledSource, {
+    filter: ['**/*.js', '**/*.jsw', '!**/*.spec.js'],
+  });
   await rsync(`${compiledSource}/`, getDirectoryFromCaller(`./${to}`));
 }
