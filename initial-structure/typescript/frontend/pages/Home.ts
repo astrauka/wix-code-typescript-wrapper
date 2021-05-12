d562ea38f1fe2501ca2de44fa50ee1acbfd5efd7
Karolis Astrauka <karolisa@wix.com>
2021-05-12 11:39:02 +0300

    Update names

--- initial-structure/typescript/frontend/pages/Home.ts
+++ initial-structure/typescript/frontend/pages/Home.ts
@@ -1,5 +1,5 @@
 // Corvid generates page names with unique suffix, we're not repeating it in the file name.
-// This page will be copied to e.g. "src/pages/Todos.zvcx/Todos.js
+// This page will be copied to e.g. "src/pages/Home.zvcx/Home.js"
 import { pick } from 'lodash';
 import { getTodoItems } from 'backend/backend-api';
 import { getDefaultId } from 'public/universal/todo-item';
