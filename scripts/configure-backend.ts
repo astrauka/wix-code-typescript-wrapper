9af91aa108d1fbc7edda2d61543bb23a63427e8d
Karolis Astrauka <karolisa@wix.com>
2021-06-25 15:37:29 +0300

    Sync config properly

--- scripts/configure-backend.ts
+++ scripts/configure-backend.ts
@@ -4,6 +4,6 @@ import { copyFromSource, copyFromTemplate, renameInDist } from './utils/files';
 export async function configureBackend() {
   await renameInDist('backend/backend-api.js', 'backend/backend-api.jsw');
   await copyFromTemplate('tsconfig.backend.json', 'backend/tsconfig.json');
-  await copyFromSource('backend', 'dist/backend', { filter: '*.config' });
+  await copyFromSource('backend', 'backend', { filter: '*.config' });
   await syncDirectory('backend', 'src/backend');
 }
